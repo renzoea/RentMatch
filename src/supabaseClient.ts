d1aa7295bfa13da48aca9ec6e7a22c185027f64e
Pablo Agustín Giampetruzzi <pablogiampe@gmail.com>
2025-09-08 20:49:21 -0300

    borramos todo

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-// filepath: src/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://<your-project>.supabase.co'
-const supabaseKey = '<your-anon-key>'
-export const supabase = createClient(supabaseUrl, supabaseKey)
