43b62e579d2be8efbc1a7bf25f4258eab83ba2ec
Pablo Agustín Giampetruzzi <pablogiampe@gmail.com>
2025-09-11 21:31:40 -0300

    cambios

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -67,6 +67,14 @@ export default function Home() {
           />
           Learn
         </a>
+        <Link href="/login">
+  <button>Ir al Login</button>
+</Link>
+
+// O directamente como enlace
+<Link href="/dashboard" className="text-blue-500">
+  Ir al Dashboard
+</Link>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
