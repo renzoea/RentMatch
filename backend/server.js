b9c3874287083374b2e56a83082828465e18e17a
David da Silva <daviddf2497@gmail.com>
2025-09-23 23:00:46 -0300

    test

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'http://localhost:3000',
+    //'http://localhost:3000',
     'https://rent-match-umber.vercel.app'
   ],
   credentials: true
