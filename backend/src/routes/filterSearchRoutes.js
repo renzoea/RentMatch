845123647f81d389b443a8ce9a25887e71e2c247
renzoea <aezequiel449@gmail.com>
2025-10-09 20:18:10 -0300

    Filtros propietario terminados

--- backend/src/routes/filterSearchRoutes.js
+++ backend/src/routes/filterSearchRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {autenticateToken } = require('../middleware/authMiddleware');
+const {authenticateToken, authorizeOwner } = require('../middleware/authMiddleware');
 const { GetAllSearch, FilterByType, FilterByRoomsRange, FilterByBathrooms, FilterByPriceRange, FilterByLeaseDuration, FilterBychildren,FilterByFurnished, 
     FilterByPets, FilterByAmenities, FilterBySmoking, FilterByBedroomsRange,FilterByCities, FilterByNeighborhood, FilterByBalcony,
     FilterByTerrace, FliterByOccupants, FilterByVerificatedUser, FilterByElevator, FilterBySecurity, FilterByArea
@@ -7,26 +7,26 @@ const { GetAllSearch, FilterByType, FilterByRoomsRange, FilterByBathrooms, Filte
 
 const router = express.Router();
 
-router.get('/', GetAllSearch);
-router.get('/type/:type', FilterByType);
-router.get('/rooms/:max/:min', FilterByRoomsRange);
-router.get('/bathrooms/:max/:min', FilterByBathrooms);
-router.get('/bedrooms/:max/:min', FilterByBedroomsRange);
-router.get('/price/:max/:min', FilterByPriceRange);
-router.get('/lease/:duration', FilterByLeaseDuration);
-router.get('/children/:children', FilterBychildren);
-router.get('/furnished/:furnished', FilterByFurnished);
-router.get('/pets/:pets', FilterByPets);
-router.get('/amenities/:amenities', FilterByAmenities);
-router.get('/smoking/:smoking', FilterBySmoking);
-router.get('/city/:city', FilterByCities);
-router.get('/neighborhood/:neighborhood', FilterByNeighborhood);
-router.get('/balcony/:balcony', FilterByBalcony);
-router.get('/terrace/:terrace', FilterByTerrace);
-router.get('/occupants/:occupants', FliterByOccupants);
-router.get('/verificated/:verificated', FilterByVerificatedUser);
-router.get('/elevator/:elevator', FilterByElevator);
-router.get('/security/:security', FilterBySecurity);
-router.get('/area/:max/:min', FilterByArea);
+router.get('/',authenticateToken,authorizeOwner, GetAllSearch);
+router.get('/type/:type',authenticateToken,authorizeOwner, FilterByType);
+router.get('/rooms/:max/:min',authenticateToken,authorizeOwner, FilterByRoomsRange);
+router.get('/bathrooms/:max/:min',authenticateToken,authorizeOwner, FilterByBathrooms);
+router.get('/bedrooms/:max/:min',authenticateToken,authorizeOwner, FilterByBedroomsRange);
+router.get('/price/:max/:min',authenticateToken,authorizeOwner, FilterByPriceRange);
+router.get('/lease/:duration',authenticateToken,authorizeOwner, FilterByLeaseDuration);
+router.get('/children/:children',authenticateToken,authorizeOwner, FilterBychildren);
+router.get('/furnished/:furnished',authenticateToken,authorizeOwner, FilterByFurnished);
+router.get('/pets/:pets',authenticateToken,authorizeOwner, FilterByPets);
+router.get('/amenities/:amenities',authenticateToken,authorizeOwner, FilterByAmenities);
+router.get('/smoking/:smoking',authenticateToken,authorizeOwner, FilterBySmoking);
+router.get('/city/:city',authenticateToken,authorizeOwner, FilterByCities);
+router.get('/neighborhood/:neighborhood',authenticateToken,authorizeOwner, FilterByNeighborhood);
+router.get('/balcony/:balcony',authenticateToken,authorizeOwner, FilterByBalcony);
+router.get('/terrace/:terrace',authenticateToken,authorizeOwner, FilterByTerrace);
+router.get('/occupants/:occupants',authenticateToken,authorizeOwner, FliterByOccupants);
+router.get('/verificated/:verificated',authenticateToken,authorizeOwner, FilterByVerificatedUser);
+router.get('/elevator/:elevator',authenticateToken,authorizeOwner, FilterByElevator);
+router.get('/security/:security',authenticateToken,authorizeOwner, FilterBySecurity);
+router.get('/area/:max/:min',authenticateToken,authorizeOwner, FilterByArea);
 
 module.exports = router;
